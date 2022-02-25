5d5cfe1307a4da5bdb32a8312c0b0032c68766b6
Jean Sousa <jean.sousa1230@gmail.com>
2022-02-24 23:19:26 -0300

    Implementação dos Controllers

--- src/desafio3/financys/src/main/java/trilha/back/financys/controllers/CategoriaController.java
+++ src/desafio3/financys/src/main/java/trilha/back/financys/controllers/CategoriaController.java
@@ -1,4 +1,53 @@
 package trilha.back.financys.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import trilha.back.financys.domains.Categoria;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Long> create(@RequestBody Categoria categoria){
+        categoria.setId(categoria.getId());
+
+        return ResponseEntity
+                .created(null)
+                .body(categoria.getId());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> read(){
+        Categoria c1 =
+                new Categoria(
+                        1L,
+                        "Vestuário",
+                        "Aquisição de roupas");
+
+        Categoria c2 = new Categoria();
+        c2.setId(2L);
+        c2.setNome("Salário");
+        c2.setDescricao("Salário mensal referente ao mês de maio");
+
+
+        setCategorias(Arrays.asList(c1, c2));
+        return ResponseEntity.ok(getCategorias());
+    }
+
 }
--- src/desafio3/financys/src/main/java/trilha/back/financys/controllers/LancamentoController.java
+++ src/desafio3/financys/src/main/java/trilha/back/financys/controllers/LancamentoController.java
@@ -1,4 +1,12 @@
 package trilha.back.financys.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import trilha.back.financys.domains.Lancamento;
+
+@RestController
+@RequestMapping("/lancamento")
 public class LancamentoController {
+
 }

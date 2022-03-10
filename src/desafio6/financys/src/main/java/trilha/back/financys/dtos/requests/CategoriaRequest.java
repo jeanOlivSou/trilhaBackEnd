0513d90dda59347d6ede5d49ca283eb64758b05b
Jean Sousa <jean.sousa1230@gmail.com>
2022-03-10 17:29:58 -0300

    Criação de classes DTOs e Mappers

---
+++ src/desafio6/financys/src/main/java/trilha/back/financys/dtos/requests/CategoriaRequest.java
@@ -0,0 +1,51 @@
+package trilha.back.financys.dtos.requests;
+
+public class CategoriaRequest {
+
+        private Long id;
+        private String nome;
+        private String descricao;
+
+        public CategoriaRequest() {
+        }
+
+
+        public CategoriaRequest(Long id, String nome, String descricao) {
+            this.id = id;
+            this.nome = nome;
+            this.descricao = descricao;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String descricao) {
+            this.descricao = descricao;
+        }
+
+        @Override
+        public String toString() {
+            return "{\n" +
+                    "id= " + id + ",\n" +
+                    "nome= '" + nome + "', \n" +
+                    "descricao= '" + descricao + "' \n" +
+                    "}";
+        }
+    }
